b22481e454b4af22ffa3ff44e58cee26da674009
Kyaw Ye Lwin <kyawyealwin2017@gmail.com>
2024-10-25 10:26:09 +0700

    carousel updated

--- src/body/Carousel.js
+++ src/body/Carousel.js
@@ -1,37 +1,32 @@
 import React, { useEffect, useState } from 'react';
-
-const Carousel = () => {
-  const images = [
-    {path:'/images/My_Photo.jpg',description :<label>I'm <span className='text-6xl font-oswald'>Kyaw Ye Lwin</span><br/><span>I have over </span><span>7+</span><span>years experience in software developement.</span></label>},
-    {path:'/images/My_Photo.jpg',description :"Over 3 years of working experience in Software Development field as Project Leader. Strong experience in planning, scheduling, budgeting, execution, and delivery of software and web project."},
-    {path:'/images/My_Photo.jpg',description :"I'm extremely motivate person to share my knowledges. So I have some online courses. Don't worry all is free."},
-    {path:'/images/My_Photo.jpg',description :"I give software development service also"},
-  ];
-
+function Carousel () {
   const [currentIndex, setCurrentIndex] = useState(0);
-
+  const images = [
+    {path:'/images/Carousel1.jpg',description :<label>I'm <span className='text-6xl font-oswald text-lime-400'>Kyaw Ye Lwin</span><br/><span>I have over </span><span>7+</span><span> years experience in software developement.</span></label>},
+    {path:'/images/My_Photo.jpg',description :<label>Over 3 years of working experience in Software Development field as Project Leader.<br/>Strong experience in planning, scheduling, budgeting, execution, and delivery of software and web project.</label>},
+    {path:'/images/FOC.jpg',description :<label>I'm extremely motivate person to share my knowledges.<br/>So I have some online courses.<br/> Don't worry all are free.</label>},
+    {path:'/images/Kingdom.png',description :<label>I have software development services also.</label>},
+  ];
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 10000); // Change image every 5 seconds
+    }, 5000); // Change image every 5 seconds
 
     return () => clearInterval(interval); // Cleanup on unmount
   }, [images.length]);
 
   return (
-    <div className="relative overflow-hidden ml-10">
-        <div className='flex transition-transform duration-700' style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-            {images.map((image,index) =>(
-                <div key={index} className='flex-shrink-0 w-full columns-2'>
-                    <div>
-                        <img src={image.path}/>
-                    </div>
-                    <div className='flex bg-amber-600 h-full items-center justify-center'>
-                        <label className='flex font-bla text-justify leading-10 indent-8 justify-self-center justify-center w-3/4 text-slate-100 '>{image.description}</label>
-                    </div>
-                </div>
-            ))}
-        </div>
+    <div className='max-w-[1400px] h-[580px] w-full m-auto relative group'>
+    <div
+      style={{ backgroundImage: `url(${images[currentIndex].path})` }}
+      className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+    > 
+    </div>
+    <div className='rounded-2xl absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
+    <h1 className="text-white text-4xl font-bold">
+              {images[currentIndex].description}
+            </h1>
+    </div>
   </div>
   );
 };
